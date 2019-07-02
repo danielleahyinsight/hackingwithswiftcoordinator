f3b491942f58a4d703336568b98ea37af4b9826d
daniel leahy <danleahy@gmail.com>
2019-07-02 13:13:01 +0100

    Initial Commit

---
+++ Coordinators/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinators
+//
+//  Created by daniel leahy on 02/07/2019.
+//  Copyright © 2019 ucd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
